fb4fb92748e4d79ad7d5f6b1251e5457b00f7028
Kutralee-Bhava <singletake.playback@gmail.com>
2025-05-23 17:55:53 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
+});
+
 
