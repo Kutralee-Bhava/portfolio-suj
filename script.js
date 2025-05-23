d4c38d94545ff7c15e5b560d99673d2a33d5523e
Kutralee-Bhava <singletake.playback@gmail.com>
2025-05-23 18:51:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    document.body.classList.add("scrolled");
-  } else {
-    document.body.classList.remove("scrolled");
-  }
-});
-
-
